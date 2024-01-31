e989b7bb97f7e9dedb2b805e6e1b25e47036cfa8
bhamidou <badrhamidou@gmail.com>
2024-01-31 09:39:36 +0100

    fix app/server and package.json

--- back/app/app.js
+++ back/app/app.js
@@ -1,34 +1,6 @@
-const express = require('express');
-// const body_parser = require('body-parser');
-const cors = require('cors');
+require('dotenv').config()
 
-class Server {
+const Server = require('./server');
 
-    constructor() {
-        this.app = express();
-        this.userRoutePath = '/api';
-
-
-        this.middlewares();
-        this.routes();
-        
-    }
-
-    middlewares() {
-        this.app.use(cors());
-        this.app.use(express.json());
-    }
-
-    routes(){
-        //this.app.use(this.userRoutePath , require('../routes/userRoutes'));
-
-    }
-
-    listen() {
-        this.app.listen(process.env.PORT, () => {
-            console.log(`Servidor escuchando en: ${process.env.PORT}`);
-        })
-    }
-}
-
-module.exports = Server;
+const server = new Server();
+server.listen();
