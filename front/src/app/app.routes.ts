69ac591c7b9bd00afca8d96484ac4377cf7a76e3
bhamidou <badrhamidou@gmail.com>
2024-02-04 21:51:43 +0100

    implements login, show users

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'users', component: UsersComponent },
+];
