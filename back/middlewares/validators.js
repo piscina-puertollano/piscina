9846bf7615c7bb74b3db61fe5d0e3258e13ebcfe
Manuel Garcia <m.gd@outlook.es>
2024-02-08 18:25:48 +0100

    Actualizamos rutas con validator en tabla clase

---
+++ back/middlewares/validators.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+
+const validateFilds = ( req, res, next ) => {
+
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ){
+        return res.status(400).json(errors);
+    }
+
+    next();
+}
+
+module.exports = {
+    validateFilds
+}
