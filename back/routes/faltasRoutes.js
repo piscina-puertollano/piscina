b99080c49aea040035ea5d1c163d879f82163f04
Manuel Garcia <m.gd@outlook.es>
2024-02-08 20:00:17 +0100

    añadiendo validators y middlewares en las rutas

--- back/routes/faltasRoutes.js
+++ back/routes/faltasRoutes.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const router = express.Router();
+const {Router } = require('express');
 const FaltasController = require('../controllers/faltasController.js');
+const { check } = require('express-validator');
+const { validateFilds } = require('../middlewares/validators');
+const router = Router();
+
 
 router.get('/faltas', FaltasController.obtenerFaltas);
 router.get('/faltas/:id', FaltasController.obtenerFaltaPorId);
-router.post('/faltas', FaltasController.crearFalta);
+router.post('/faltas',
+[
+    check('id_usuario', 'El id del usuario debe de ser un número entero').isInt(),
+    check('id_clase', 'El id de la clase debe de ser un número entero').isInt(),
+    validateFilds
+], 
+FaltasController.crearFalta);
 router.put('/faltas/:id', FaltasController.actualizarFalta);
 router.delete('/faltas/:id', FaltasController.eliminarFalta);
 
