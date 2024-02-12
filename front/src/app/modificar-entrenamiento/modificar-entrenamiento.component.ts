05c26bda5bd960f443ce5e69f739cdf0e9cfccde
Marina Laguna <marinalaguna2004@gmail.com>
2024-02-12 13:55:16 +0100

    modificación de entrenamientos funcional

--- front/src/app/modificar-entrenamiento/modificar-entrenamiento.component.ts
+++ front/src/app/modificar-entrenamiento/modificar-entrenamiento.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Entrenamiento } from '../interfaces/entrenamiento';
 import { Alert } from '../interfaces/alert';
 import { EntrenamientoService } from '../services/entrenamiento.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AlertComponent } from '../utils/alert/alert.component';
 import { FormsModule } from '@angular/forms';
 
@@ -18,15 +18,24 @@ export class ModificarEntrenamientoComponent {
   alert: Alert;
   arrEntrenamientos: Array<Entrenamiento> = [];
 
-  constructor(private entrenamientoService: EntrenamientoService, private router: Router){
+  constructor(private entrenamientoService: EntrenamientoService, private router: Router, private route: ActivatedRoute){
     this.entrenamiento = {};
     this.alert = new Alert();
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const entrenamientoId = params['id']; 
+      this.entrenamiento.id = entrenamientoId;
+      this.getEntrenamiento();
+    });
   }
 
   updateEntrenamiento(){
     this.entrenamientoService.updateEntrenamientos(this.entrenamiento).subscribe({
       next: (entrenamiento: any | undefined) => {
-        this.entrenamiento = entrenamiento
+        this.entrenamiento = entrenamiento;
+        this.router.navigate(['/entrenamientos']);
       },
       error: (err) => {
         console.log(err)
@@ -36,19 +45,19 @@ export class ModificarEntrenamientoComponent {
 
   getEntrenamiento() {
     this.entrenamientoService.getEntrenamientoId({ id: this.entrenamiento.id }).subscribe({
-      next: (entrenamiento: any | undefined) => {
-        console.log(entrenamiento);
-        if (!entrenamiento || entrenamiento.length ===  0) {
+      next: (response: any) => {
+        console.log('Respuesta completa:', response);
+        if (response && !Array.isArray(response)) {
+          this.entrenamiento = response; // Asume que la respuesta es un objeto
+        } else {
           this.alert.show = true;
           this.alert.header = 'Error';
           this.alert.message = 'Entrenamiento no encontrado';
-        } else {
-          this.alert.show = false;
-          this.entrenamiento = entrenamiento[0]; 
+          this.entrenamiento = {}; // Reinicia el objeto entrenamiento a un estado vacío
         }
       },
       error: (err) => {
-        console.log(err);
+        console.error('Error al obtener el entrenamiento:', err);
         this.alert.show = true;
         this.alert.header = 'Error';
         this.alert.message = 'Ha ocurrido un error al intentar obtener el entrenamiento';
