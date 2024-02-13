1584fd22689d18631864db6d5056b75ee1fb975f
bhamidou <badrhamidou@gmail.com>
2024-02-13 19:57:30 +0100

    implement view landing and edit club

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { LoginComponent } from './users/login/login.component';
 import { UsersComponent } from './users/admin/users.component';
 import { ClaseComponent} from "./clase/clase.component";
 import { EntrenamientoComponent } from './entrenamiento/entrenamiento.component';
+import { ListComponent } from './users/list/list.component';
+import { ClubComponent } from './landing/club/club.component';
+import { ClubEditComponent } from './editor/club/club.component';
 
 export const routes: Routes = [
+  {path: '', component: ClubComponent },
   {path: 'login', component: LoginComponent },
-  {path: 'users', component: UsersComponent },
+  {path: 'users', component: ListComponent },
   {path: 'clases', component: ClaseComponent},
   {path: 'entrenamientos', component: EntrenamientoComponent},
+  {path: 'edit', component: ClubEditComponent},
 ];
--- front/src/app/interfaces/user.ts
+++ front/src/app/interfaces/user.ts
@@ -1,9 +1,18 @@
 export interface User {
-    id?:number
-    idSocio?:number
-    firstName?:string
-    lastName?:string
-    email?:string
-    password?:string
-    roles?:Array<any>
+    id?:        number;
+    idSocio?:   number;
+    firstName?: string;
+    lastName?:  string;
+    password?:  string;
+    email?:     string;
+    roles?:     Role[];
+    image?:     Image | null;
+}
+
+export interface Image {
+    ruta: string;
+}
+
+export interface Role {
+    id: number;
 }
