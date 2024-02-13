c1b2c19568b49bdb585b386e13f9492aca6e5a12
Manuel Garcia <m.gd@outlook.es>
2024-02-13 10:59:18 +0100

    Añadida funcionalidad eliminar clases

--- front/src/app/clase/clase.component.ts
+++ front/src/app/clase/clase.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-clase',
+  standalone: true,
+  imports: [TableModule],
   templateUrl: './clase.component.html',
   styleUrls: ['./clase.component.css']
 })
 export class ClaseComponent implements OnInit {
   datos: any[] = [];
+  selectedUser: any;
+  currentNombre: string = "";
+  currentTemporada: string = "";
+  isEditing: boolean = false;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.cargarDatos();
@@ -16,22 +24,36 @@ export class ClaseComponent implements OnInit {
 
   cargarDatos(): void {
     const apiUrl = 'http://localhost:9090/api/clases';
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
+    this.http.get<any[]>(apiUrl).subscribe(
+      data => {
         this.datos = data;
-      })
-      .catch(error => {
+      },
+      error => {
         console.error('Hubo un error al obtener los datos:', error);
-      });
+      }
+    );
   }
 
-  trackByNombre(index: number, user: any): string | undefined {
-    return user?.nombre; // Suponiendo que 'nombre' es una propiedad única de cada usuario
+  prepareEdit(user: any): void {
+    this.currentNombre = user.nombre;
+    this.currentTemporada = user.temporada;
+    this.isEditing = true;
+  }
+
+  saveChanges(): void {
+    // Implementa la lógica para guardar los cambios
+    // Por ejemplo, puedes hacer una petición HTTP para actualizar los datos en el servidor
+  }
+
+  eliminar(user: any): void {
+    const apiUrl = `http://localhost:9090/api/clases/${user.id}`;
+    this.http.delete(apiUrl)
+      .subscribe(() => {
+        // Elimina la clase del arreglo local después de una respuesta exitosa
+        this.datos = this.datos.filter(clase => clase.id !== user.id);
+      }, error => {
+        // Maneja el error, por ejemplo, mostrando un mensaje al usuario
+        console.error('Error al eliminar la clase:', error);
+      });
   }
 }
