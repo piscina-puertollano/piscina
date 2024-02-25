64bb32ba49578cad45a91191aae73e0e25657265
bhamidou <badrhamidou@gmail.com>
2024-02-25 16:06:00 +0100

    implements file service

---
+++ front/src/app/services/file.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import { File } from '../interfaces/upload';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  constructor(private http: HttpClient) { }
+  url = environment.baseUrl+environment.file
+
+  showImage(image: File):Observable<Blob|undefined> {
+    return this.http.post(this.url+'/'+image.id,{folder:image.where},{responseType: 'blob'}).pipe(
+      catchError((error) =>{
+        return of(error)
+      })
+    )
+  }
+
+  deleteImage(image: File):Observable<File|undefined> {
+    let urldelete = this.url+'/'+image.id+'/'+image.where
+    return this.http.delete(urldelete).pipe(
+      catchError((error) =>{
+        return of(error)
+      })
+    )
+  }
+}
