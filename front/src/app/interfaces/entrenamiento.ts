90a206e7e7727434c33e51dcee2fb42be56b39ec
Marina Laguna <marinalaguna2004@gmail.com>
2024-02-25 18:02:18 +0100

    correción en la pantalla de modificación de entrenamientos con sus respectivos ejercicios

--- front/src/app/interfaces/entrenamiento.ts
+++ front/src/app/interfaces/entrenamiento.ts
@@ -8,6 +8,6 @@ export interface Entrenamiento {
     id?:number
     nombre?:string
     descripcion?:string
-    ejercicios?:Array<Ejercicios>
+    ejercicios?: Ejercicios[]
     EjercicioEntrenamientos?: EjercicioEntrenamiento[];
 }
