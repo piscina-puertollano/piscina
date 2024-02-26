3ee568eafd7c5ee04894120ed2d247694a3d1c74
bhamidou <badrhamidou@gmail.com>
2024-02-26 02:03:40 +0100

    implements jest and some test

---
+++ back/tests/userRoutes.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+const express = require('express');
+const routes = require('../routes/userRoutes')
+/**
+ * @author: badr
+ */
+const app = express();
+app.use(express.json());
+app.use('/', routes);
+
+
+describe('POST /login', () => {
+  it('POST /login debería retornar un código 200 con token', async () => {
+    
+    const user = {
+        email: "admin@piscina.com",
+        password: "1234"
+      }
+      const response = await request(app).post('/login').send(user) 
+      console.log(response.body)
+      expect(response.statusCode).toBe(200);
+      expect(response.body.token).toBeTruthy();
+
+      const newUser = {
+        firstName: "fernando",
+        lastName: "zabe",
+        email: "faranzabe@piscina.com",
+        password: "1234"
+      }
+
+      //con token no tine que fallar - badr
+      const response2 = await request(app).post('/user').send(newUser).set({'x-token':response.body.token})
+      console.log(response2.body)
+      expect(response2.statusCode).toBe(200);
+
+
+    });
+
+
+});
+
+describe('POST /user', () => {
+    //sin token tine que fallar - badr
+    it('POST /user debería retornar un código 400', async () => {
+      
+      const user = {
+          email: "admin@piscina.com",
+          password: "1234"
+        }
+        const response = await request(app).post('/user').send(user) 
+        console.log(response.body)
+        expect(response.statusCode).toBe(401);
+  
+      });
+  });
+  
