e16283b5e548811b16a1577331443e90220e0c79
Manuel Garcia <m.gd@outlook.es>
2024-02-27 13:10:50 +0100

    creada pantalla crear faltas

---
+++ front/src/app/faltas/crear-faltas/crear-faltas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-faltas',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-faltas.component.html',
+  styleUrl: './crear-faltas.component.css'
+})
+export class CrearFaltasComponent {
+
+}
