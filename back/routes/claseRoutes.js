f2a923555bac92da73b4e1f5adcf7987148aedf2
Manuel Garcia <m.gd@outlook.es>
2024-03-03 16:45:56 +0100

    visualizamos datos asignacion usuarios

--- back/routes/claseRoutes.js
+++ back/routes/claseRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const ClaseController = require('../controllers/claseController.js');
-const { checkToken, tokenCanTrainer } = require('../middlewares/abilities.js');
+const { checkToken, tokenCanTrainer, tokenCanAdmin } = require('../middlewares/abilities.js');
 
 // Define the routes
-router.get('/clases', [checkToken, tokenCanTrainer], ClaseController.obtenerClases);
-router.get('/clases/:id', [checkToken, tokenCanTrainer], ClaseController.obtenerClasePorId);
-router.get('/clases/temporada/:temporada', [checkToken, tokenCanTrainer], ClaseController.obtenerTemporadaPorId);
-router.post('/clases', [checkToken, tokenCanTrainer], ClaseController.crearClase);
-router.put('/clases/:id', [checkToken, tokenCanTrainer], ClaseController.actualizarClase);
-router.delete('/clases/:id', [checkToken, tokenCanTrainer], ClaseController.eliminarClase);
+router.get('/clases', [checkToken, tokenCanAdmin], ClaseController.obtenerClases);
+router.get('/clases/:id', [checkToken, tokenCanAdmin], ClaseController.obtenerClasePorId);
+router.get('/clases/temporada/:temporada', [checkToken, tokenCanAdmin], ClaseController.obtenerTemporadaPorId);
+router.post('/clases', [checkToken, tokenCanAdmin], ClaseController.crearClase);
+router.put('/clases/:id', [checkToken, tokenCanAdmin], ClaseController.actualizarClase);
+router.delete('/clases/:id', [checkToken, tokenCanAdmin], ClaseController.eliminarClase);
 
 // Export the router
 module.exports = router;
--- front/src/app/asignar-clases-usuarios/asignar-clases-usuarios.component.ts
+++ front/src/app/asignar-clases-usuarios/asignar-clases-usuarios.component.ts
@@ -1,18 +1,20 @@
-import { ClasehasusuarioService } from './../services/clasehasusuario.service';
-import { claseUsuario } from './../interfaces/claseUsuario';
-import { User } from './../interfaces/user';
 /**
  * author: Manuel Garcia
  */
+
+import { ClasehasusuarioService } from './../services/clasehasusuario.service';
+import { claseUsuario } from './../interfaces/claseUsuario';
+import { User } from './../interfaces/user';
 import { ButtonModule } from 'primeng/button';
 import { Router, RouterLink } from '@angular/router';
-import { TableModule} from 'primeng/table';
+import { TableModule } from 'primeng/table';
 import { Component, OnInit } from '@angular/core';
 import { ClaseService } from '../services/clase.service';
 import { Alert } from '../interfaces/alert';
 import { FormsModule } from '@angular/forms';
 import { Clase } from '../interfaces/clase';
 import { AlertComponent } from '../utils/alert/alert.component';
+import moment from 'moment';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { DialogModule } from 'primeng/dialog';
@@ -21,71 +23,147 @@ import { DropdownModule } from 'primeng/dropdown';
 import { CategoriaClasesService } from '../services/categoria-clases.service';
 import { CategoriaClases } from '../interfaces/categoriaClases';
 import { UserService } from '../services/user.service';
+import { asignacionClasesUsuario } from '../interfaces/asignacionClases';
 @Component({
   selector: 'app-asignar-clases-usuarios',
   standalone: true,
-  imports: [TableModule, FormsModule, AlertComponent, ToastModule, DropdownModule, ButtonModule, InputTextModule, DialogModule, RouterLink, InputTextareaModule],
+  imports: [
+    TableModule,
+    FormsModule,
+    AlertComponent,
+    ToastModule,
+    DropdownModule,
+    ButtonModule,
+    InputTextModule,
+    DialogModule,
+    RouterLink,
+    InputTextareaModule,
+  ],
   templateUrl: './asignar-clases-usuarios.component.html',
-  styleUrl: './asignar-clases-usuarios.component.css'
+  styleUrl: './asignar-clases-usuarios.component.css',
 })
 export class AsignarClasesUsuariosComponent implements OnInit {
+  user: User;
   alert: Alert;
+  arrUsers: Array<User> = [];
   searchValue: string = '';
   whatSearch: string = '';
-
+ 
   clase: Clase;
   claseUsuario: claseUsuario;
   arrClaseUsuario: Array<claseUsuario> = [];
-
-  constructor(private service: ClaseService, private UsuarioService: UserService, private ClasehasusuarioService: ClasehasusuarioService, private relacion: ClasehasusuarioService, private router: Router) {
-    this.alert = new Alert();
-    this.clase = {}
-    this.claseUsuario = {}
-    
- }
-
- ngOnInit(): void {
-  this.allClasesUsuarios();
- }
- allClasesUsuarios() {
-  this.ClasehasusuarioService.allRelacion().subscribe({
-    next: (categoria: any | undefined) => {
-      console.log(categoria);
-      if (categoria.status >= 400) {
-        this.alert.show = true;
-        this.alert.header = 'Error';
-        this.alert.message =
-          'No se han podido cargar la informacion. Póngase en contacto con un administrador.';
-      } else {
-        this.arrClaseUsuario = categoria;
-        console.log(this.arrClaseUsuario)
-      }
-    },
-    error: (err) => {
-      console.log(err);
-    },
-  });
-}  
-
-  deleteRelacion(id: any) {
-    this.ClasehasusuarioService.deleteRelacion(id).subscribe({
-      next: (clase: any | undefined) => {
-        console.log(clase);
-        if (clase.length == 0 || clase.status == 404) {
-          this.alert.show = true;
-          this.alert.header = 'Error';
-          this.alert.message = 'El usuario no se ha podido eliminar';
-        } else {
-          this.alert.show = true;
-          this.alert.header = 'Operación completada';
-          this.alert.message = 'Usuario eliminado correctamente';
-          //this.alert.type = 'success'
-          location.reload();
-        }
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+  arrClases: Array<Clase> = [];
+ 
+  resultadoRelacion: asignacionClasesUsuario[] = [];
+ 
+  constructor(
+     private service: ClaseService,
+     private UsuarioService: UserService,
+     private ClasehasusuarioService: ClasehasusuarioService,
+     private router: Router
+  ) {
+     this.alert = new Alert();
+     this.clase = {};
+     this.claseUsuario = {};
+     this.user = {};
   }
-}
+ 
+  ngOnInit(): void {
+    this.allUsers();
+    this.allClases();
+   }
+   
+   allUsers() {
+    this.UsuarioService.allUsers().subscribe({
+       next: (user: any | undefined) => {
+         if (user.status >= 400) {
+           this.alert.show = true;
+           this.alert.header = 'Error';
+           this.alert.message = 'No se han podido cargar a los usuarios. Póngase en contacto con un adminsitrador.';
+         } else {
+           this.arrUsers = user;
+           this.allClases(); // Llamamos a allClases después de cargar usuarios
+         }
+       },
+       error: (err) => {
+         console.log(err);
+       },
+    });
+   }
+   
+   allClases() {
+    this.service.allClases().subscribe({
+       next: (clase: any | undefined) => {
+         if (clase.status >= 400) {
+           this.alert.show = true;
+           this.alert.header = 'Error';
+           this.alert.message = 'No se han podido cargar la informacion. Póngase en contacto con un adminsitrador.';
+         } else {
+           this.arrClases = clase;
+           this.allClasesUsuarios(); // Llamamos a allClasesUsuarios después de cargar clases
+         }
+       },
+       error: (err) => {
+         console.log(err);
+       },
+    });
+   }
+   
+   allClasesUsuarios() {
+    this.ClasehasusuarioService.allRelacion().subscribe({
+       next: (categoria: any | undefined) => {
+         if (categoria.status >= 400) {
+           this.alert.show = true;
+           this.alert.header = 'Error';
+           this.alert.message = 'No se han podido cargar la informacion. Póngase en contacto con un administrador.';
+         } else {
+           this.arrClaseUsuario = categoria;
+           this.resultadoRelacion = []; // Inicializamos el array resultadoRelacion
+   
+           for (let i = 0; i < this.arrClaseUsuario.length; i++) {
+             const clases = this.arrClaseUsuario[i];
+             const user = this.arrUsers.find(
+               (user) => user.id === clases.id_usuario
+             );
+             const clase = this.arrClases.find(
+               (clase) => clase.id === clases.id_clase
+             );
+   
+             const relacion = {
+               id: clases.id,
+               id_usuario: clases.id_usuario,
+               nombre_usuario: user?.firstName,
+               id_clase: clases.id_clase,
+               nombre_clase: clase?.nombre
+             };
+             this.resultadoRelacion.push(relacion);
+           }
+           console.log(this.resultadoRelacion);
+         }
+       },
+       error: (err) => {
+         console.log(err);
+       },
+    });
+   }
+   
+  deleteRelacion(id: any) {
+     this.ClasehasusuarioService.deleteRelacion(id).subscribe({
+       next: (clase: any | undefined) => {
+         if (clase.length == 0 || clase.status == 404) {
+           this.alert.show = true;
+           this.alert.header = 'Error';
+           this.alert.message = 'El usuario no se ha podido eliminar';
+         } else {
+           this.alert.show = true;
+           this.alert.header = 'Operación completada';
+           this.alert.message = 'Usuario eliminado correctamente';
+           location.reload();
+         }
+       },
+       error: (err) => {
+         console.log(err);
+       },
+     });
+  }
+ }
