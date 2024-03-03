dd669bf7af93c4b90f7cebbf48519c5fd4aed49b
Manuel Garcia <m.gd@outlook.es>
2024-03-03 21:40:09 +0100

    actualizando rama con datos parametrizados

--- back/seeders/20240227095228-categoriaClases.js
+++ back/seeders/20240227095228-categoriaClases.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
    
-    await queryInterface.bulkInsert('categoria_clases', [{
+    await queryInterface.bulkInsert(process.env.TABLE_CATEGORIA_CLASES, [{
       nombre: 'Infantil',
       createdAt: new Date(),
       updatedAt: new Date()
