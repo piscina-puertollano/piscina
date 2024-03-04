5460d35ed0fc4d64c8bf1d0e2cb12b3f785b897f
Manuel Garcia <m.gd@outlook.es>
2024-03-04 16:40:12 +0100

    actualizando ts

--- front/src/app/components/asignar-clases-usuarios/asignar-clases-usuarios.component.ts
+++ front/src/app/components/asignar-clases-usuarios/asignar-clases-usuarios.component.ts
@@ -8,7 +8,7 @@ import { User } from '../../interfaces/user';
 import { ButtonModule } from 'primeng/button';
 import { Router, RouterLink } from '@angular/router';
 import { TableModule } from 'primeng/table';
-import { Component, OnInit } from '@angular/core';d
+import { Component, OnInit } from '@angular/core';
 import { ClaseService } from '../../services/clase.service';
 import { Alert } from '../../interfaces/alert';
 import { FormsModule } from '@angular/forms';
@@ -58,6 +58,17 @@ export class AsignarClasesUsuariosComponent implements OnInit {
   resultadoRelacion: asignacionClasesUsuario[] = [];
 
   displayDialogCrear: boolean = false;
+  selectedUser: any = null;
+  selectedClase: any = null;
+  clasesFiltradas: any[] = [];
+  selectedDia: string = '';
+  diasSemana = [
+    { label: 'Lunes', value: 'Lunes' },
+    { label: 'Martes', value: 'Martes' },
+    { label: 'Miércoles', value: 'Miercoles' },
+    { label: 'Jueves', value: 'Jueves' },
+    { label: 'Viernes', value: 'Viernes' },
+  ];
 
   constructor(
     private service: ClaseService,
@@ -74,14 +85,29 @@ export class AsignarClasesUsuariosComponent implements OnInit {
   ngOnInit(): void {
     this.allUsers();
     this.allClases();
+    this.allClasesUsuarios();
     this.alert = new Alert();
+    this.selectedDia = ''
   }
 
+  onDiaSeleccionado(event: any) {
+    console.log('Día seleccionado:', event.value);
+    this.clasesFiltradas = this.arrClases.filter(clase => clase.nombre === event.value);
+    for (let index = 0; index < this.clasesFiltradas.length; index++) {
+      
+      const horaInicio: moment.Moment = moment(this.clasesFiltradas[index].hora_inicio, "HH:mm:ss");
+      this.clasesFiltradas[index].hora_inicio = horaInicio.format("HH:mm");
+  }
 
+  for (const clase of this.clasesFiltradas) {
+      console.log(clase.nombre);
+  }
+ }
+   
 
   cerrarModalCrear() {
     this.displayDialogCrear = false;
-   }
+  }
 
   allUsers() {
     this.UsuarioService.allUsers().subscribe({
@@ -89,10 +115,10 @@ export class AsignarClasesUsuariosComponent implements OnInit {
         if (user.status >= 400) {
           this.alert.show = true;
           this.alert.header = 'Error';
-          this.alert.message =
-            'No se han podido cargar a los usuarios.';
+          this.alert.message = 'No se han podido cargar a los usuarios.';
         } else {
           this.arrUsers = user;
+          console.log('Usuarios ', this.arrUsers);
           this.allClases();
         }
       },
@@ -108,11 +134,9 @@ export class AsignarClasesUsuariosComponent implements OnInit {
         if (clase.status >= 400) {
           this.alert.show = true;
           this.alert.header = 'Error';
-          this.alert.message =
-            'No se han podido cargar la informacion.';
+          this.alert.message = 'No se han podido cargar la informacion.';
         } else {
           this.arrClases = clase;
-          this.allClasesUsuarios();
         }
       },
       error: (err) => {
@@ -127,8 +151,7 @@ export class AsignarClasesUsuariosComponent implements OnInit {
         if (categoria.status >= 400) {
           this.alert.show = true;
           this.alert.header = 'Error';
-          this.alert.message =
-            'No se han podido cargar la informacion.';
+          this.alert.message = 'No se han podido cargar la informacion.';
         } else {
           this.arrClaseUsuario = categoria;
           this.resultadoRelacion = []; // Inicializamos el array resultadoRelacion
@@ -179,5 +202,34 @@ export class AsignarClasesUsuariosComponent implements OnInit {
 
   abrirModalCrear() {
     this.displayDialogCrear = true;
- }
+  }
+
+  agregarRelacion() {
+    let nuevaRelacion = {
+      id_usuario: this.selectedUser,
+      id_clase: this.selectedClase
+      
+    };
+  
+    this.ClasehasusuarioService.agregarRelacion(nuevaRelacion).subscribe({
+      next: (resultado: any) => {
+        console.log(resultado);
+        if (resultado.status >=  400) {
+          this.alert.show = true;
+          this.alert.header = 'Error';
+          this.alert.message =
+            'No se ha podido insertar la nueva categoría. Póngase en contacto con un administrador.';
+        } else {
+          location.reload();
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.alert.show = true;
+        this.alert.header = 'Error';
+        this.alert.message =
+          'Ha ocurrido un error al realizar la asignacion.';
+      },
+    });
+  }  
 }
