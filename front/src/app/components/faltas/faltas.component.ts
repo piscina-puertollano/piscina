ee6d749ffe7db19e57d445dd0dcd7177def2b57e
Manuel Garcia <m.gd@outlook.es>
2024-03-05 13:20:17 +0100

    agrego funcionalidad de insertar faltas

--- front/src/app/components/faltas/faltas.component.ts
+++ front/src/app/components/faltas/faltas.component.ts
@@ -4,6 +4,7 @@
 import { User } from '../../interfaces/user';
 import { ButtonModule } from 'primeng/button';
 import { Router, RouterLink } from '@angular/router';
+import { ClasehasusuarioService } from './../../services/clasehasusuario.service';
 import { TableModule } from 'primeng/table';
 import { Component, OnInit } from '@angular/core';
 import { ClaseService } from '../../services/clase.service';
@@ -21,6 +22,8 @@ import { UserService } from '../../services/user.service';
 import { asignacionClasesUsuario } from '../../interfaces/asignacionClases';
 import { Faltas } from '../../interfaces/faltas';
 import { FaltasService } from './../../services/faltas.service';
+import { claseUsuario } from '../../interfaces/claseUsuario';
+import { MultiSelectModule } from 'primeng/multiselect';
 @Component({
   selector: 'app-faltas',
   standalone: true,
@@ -36,6 +39,7 @@ import { FaltasService } from './../../services/faltas.service';
     DialogModule,
     RouterLink,
     InputTextareaModule,
+    MultiSelectModule,
   ],
   templateUrl: './faltas.component.html',
   styleUrl: './faltas.component.css',
@@ -53,11 +57,13 @@ export class FaltasComponent implements OnInit {
   arrClases: Array<Clase> = [];
 
   resultadoRelacion: asignacionClasesUsuario[] = [];
+  resultadoUnion: claseUsuario[] = [];
 
   displayDialogCrear: boolean = false;
   selectedUser: any = null;
   selectedClase: any = null;
   clasesFiltradas: any[] = [];
+  userFiltrado: any[] = [];
   selectedDia: string = '';
   diasSemana = [
     { label: 'Lunes', value: 'Lunes' },
@@ -73,12 +79,13 @@ export class FaltasComponent implements OnInit {
     dia: null,
     clase: null,
   };
-  id: number = 0
+  id: number = 0;
 
   constructor(
     private service: ClaseService,
     private UsuarioService: UserService,
     private FaltasService: FaltasService,
+    private ClasehasusuarioService: ClasehasusuarioService,
     private router: Router
   ) {
     this.alert = new Alert();
@@ -91,6 +98,7 @@ export class FaltasComponent implements OnInit {
     this.allUsers();
     this.allClases();
     this.allClasesUsuarios();
+    this.allFaltas();
     this.alert = new Alert();
     this.selectedDia = '';
   }
@@ -100,6 +108,7 @@ export class FaltasComponent implements OnInit {
     this.clasesFiltradas = this.arrClases.filter(
       (clase) => clase.nombre === event.value
     );
+
     for (let index = 0; index < this.clasesFiltradas.length; index++) {
       const horaInicio: moment.Moment = moment(
         this.clasesFiltradas[index].hora_inicio,
@@ -107,10 +116,32 @@ export class FaltasComponent implements OnInit {
       );
       this.clasesFiltradas[index].hora_inicio = horaInicio.format('HH:mm');
     }
+  }
 
-    for (const clase of this.clasesFiltradas) {
-      console.log(clase.nombre);
+  onUsersSeleccionados(event: any) {
+    let filteredClaseUsuario = [];
+    for (let i = 0; i < this.arrClaseUsuario.length; i++) {
+      if (this.arrClaseUsuario[i].id_clase === event.value) {
+        filteredClaseUsuario.push(this.arrClaseUsuario[i]);
+      }
     }
+    console.log(filteredClaseUsuario);
+
+    let uniqueUserIds = new Set();
+    let filteredUsers = [];
+    for (let i = 0; i < filteredClaseUsuario.length; i++) {
+      for (let j = 0; j < this.arrUsers.length; j++) {
+        if (
+          filteredClaseUsuario[i].id_usuario === this.arrUsers[j].id &&
+          !uniqueUserIds.has(this.arrUsers[j].id)
+        ) {
+          filteredUsers.push(this.arrUsers[j]);
+          uniqueUserIds.add(this.arrUsers[j].id);
+          break;
+        }
+      }
+    }
+    this.userFiltrado = filteredUsers;
   }
 
   cerrarModalCrear() {
@@ -153,7 +184,7 @@ export class FaltasComponent implements OnInit {
     });
   }
 
-  allClasesUsuarios() {
+  allFaltas() {
     this.FaltasService.allFaltas().subscribe({
       next: (categoria: any | undefined) => {
         if (categoria.status >= 400) {
@@ -162,7 +193,7 @@ export class FaltasComponent implements OnInit {
           this.alert.message = 'No se han podido cargar la informacion.';
         } else {
           this.arrClaseUsuario = categoria;
-          console.log('faltas', this.arrClaseUsuario)
+          console.log('faltas', this.arrClaseUsuario);
           this.resultadoRelacion = [];
 
           for (let i = 0; i < this.arrClaseUsuario.length; i++) {
@@ -192,6 +223,23 @@ export class FaltasComponent implements OnInit {
     });
   }
 
+  allClasesUsuarios() {
+    this.ClasehasusuarioService.allRelacion().subscribe({
+      next: (categoria: any | undefined) => {
+        if (categoria.status >= 400) {
+          this.alert.show = true;
+          this.alert.header = 'Error';
+          this.alert.message = 'No se han podido cargar la informacion.';
+        } else {
+          this.arrClaseUsuario = categoria;
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
   deleteRelacion(id: any) {
     this.FaltasService.deleteFaltas(id).subscribe({
       next: () => {
@@ -213,32 +261,41 @@ export class FaltasComponent implements OnInit {
     this.displayDialogCrear = true;
   }
 
-  agregarRelacion() {
-    let nuevaRelacion = {
-      id_usuario: this.selectedUser,
-      id_clase: this.selectedClase,
-    };
-
-    this.FaltasService.agregarFalta(nuevaRelacion).subscribe({
-      next: (resultado: any) => {
-        console.log(resultado);
-        if (resultado.status >= 400) {
-          this.alert.show = true;
-          this.alert.header = 'Error';
-          this.alert.message =
-            'No se ha podido insertar la nueva categoría. Póngase en contacto con un administrador.';
-        } else {
-          location.reload();
-        }
-      },
-      error: (err) => {
-        console.log(err);
-        this.alert.show = true;
-        this.alert.header = 'Error';
-        this.alert.message = 'Ha ocurrido un error al realizar la asignacion.';
-      },
-    });
-  }
+  agregarFalta() {
+    let completadas = 0;
+    const totalInserciones = this.selectedUser.length; 
+   
+    for (let i = 0; i < this.selectedUser.length; i++) {
+       const nuevaFalta = {
+         id_usuario: this.selectedUser[i],
+         id_clase: this.selectedClase,
+         fecha: moment().toDate(),
+       };
+   
+       this.FaltasService.agregarFalta(nuevaFalta).subscribe({
+         next: (resultado: any) => {
+           console.log(resultado);
+           if (resultado.status >= 400) {
+             this.alert.show = true;
+             this.alert.header = 'Error';
+             this.alert.message = 'No se ha podido insertar la nueva falta. Póngase en contacto con un administrador.';
+           } else {
+             completadas++;
+             if (completadas === totalInserciones) {
+               location.reload();
+             }
+           }
+         },
+         error: (err) => {
+           console.log(err);
+           this.alert.show = true;
+           this.alert.header = 'Error';
+           this.alert.message = 'Ha ocurrido un error al realizar la asignación.';
+         },
+       });
+    }
+   }
+   
 
   openEditModal(claseUsuario: any) {
     this.displayDialogEditar = true;
@@ -253,7 +310,10 @@ export class FaltasComponent implements OnInit {
       id_usuario: this.editData.user,
       id_clase: this.editData.clase,
     };
-    this.FaltasService.actualizarFaltas(this.editData.id, relacionActualizada).subscribe({
+    this.FaltasService.actualizarFaltas(
+      this.editData.id,
+      relacionActualizada
+    ).subscribe({
       next: (resultado: any) => {
         console.log(resultado);
         if (resultado.status >= 400) {
