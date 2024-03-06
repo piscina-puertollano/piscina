d0602c1a764a8fd14d25e03094202ef2bb230acd
Marina Laguna <marinalaguna2004@gmail.com>
2024-03-06 14:18:31 +0100

    Modificación de los seeders

--- back/seeders/20240203201725-EntrenamientoSeeder.js
+++ back/seeders/20240203201725-EntrenamientoSeeder.js
@@ -8,10 +8,16 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert(process.env.TABLE_ENTRENAMIENTOS, [{
       nombre: '3000 metros',
-      descripcion: 'Entrenamiento de 3000 metros en las Lagunas de Ruidera',
+      descripcion: 'Entrenamiento de 3000 metros en las Lagunas de Ruidera.',
       createdAt: new Date(),
       updatedAt: new Date()
     },
+    {
+      nombre: 'Entrenamiento de Natación',
+      descripcion: 'Entrenamiento de natación de resistencia y velocidad en piscina. Incluye series de estilos libres, espalda, pecho y braza, con intervalos de descanso y calentamiento.',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
   ])
   },
 
